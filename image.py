6ccb5fb3cb59d4923599e5659fdde573629d9f8a
Khireddine Benmeziane <benmezianekhireddine273@gmail.com>
2024-06-20 11:53:33 +0200

    Update image.py

--- image.py
+++ image.py
@@ -9,10 +9,9 @@ density = " .'`^\",:;Il!i><~+_-?][}{1)(|\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B
 lenght_of_density = len(density)
 moy = 256//lenght_of_density+1
 
-font_size = 16
-my_font = ImageFont.truetype("courier.ttf", font_size)
+font_size = 32
 
-new_width = 100
+my_font = ImageFont.truetype("courier.ttf", size=font_size)
 
 def main():
     if len(sys.argv) < 2 :
@@ -40,6 +39,9 @@ def load_image(image_file):
         print(image_file + " doesn't exist, please make sure to put the right path to the image.")
         return -1
     image = Image.open(image_file)
+
+    # resize the image
+    new_width = 100 # You can choose the new width as you want and you can uncomment all the code below to return the original image
     if image.width > new_width :
         ratio = image.height / image.width
         image = image.resize((new_width, (int)(new_width*ratio)))
@@ -49,37 +51,15 @@ def load_image(image_file):
 # converting the image to an image with ascii characters
 def convert_to_ascii(resized_image, new_image_name, original_image_name):
     new_image = Image.new(mode="RGB", size=(resized_image.width*font_size, resized_image.height*font_size), color=(0, 0, 0))
+    # new_image = Image.new(mode="RGB", size=(resized_image.width*font_size, resized_image.height*font_size), color=(255, 255, 255))
     draw_in_image = ImageDraw.Draw(new_image)
 
     for y in range(0, resized_image.height):
         for x in range(0, resized_image.width):
-            r, g, b = (0, 0, 0)
             pixel = resized_image.getpixel((x,y))
+            average = (sum(pixel)//len(pixel))//moy
+            draw_in_image.text((x*font_size, y*font_size), density[average], font=my_font, fill=(pixel[0], pixel[1], pixel[2]))
 
-            # Checking the type of pixel (it can be different from an image to another)
-            if type(pixel).__name__ == "tuple" :
-                pixel_length = len(pixel)
-                # Now we are checking the pixel tuple length, a pixel can be represented by (Red value, Green value, Blue value, Alpha value) or less than that
-                if pixel_length == 4: 
-                    r, g, b, a = pixel
-                    if a == 0:
-                        continue # it is transparent we don't have to write anything
-                elif pixel_length == 3:
-                    r, g, b = pixel
-                elif pixel_length == 2:
-                    r, g = pixel
-            else:
-                r = pixel
-
-            average = 0
-
-            if type(pixel).__name__ == "tuple" :
-                average = ((r+g+b)//pixel_length)//moy
-            else :
-                average = r//lenght_of_density
-
-            draw_in_image.text((x*font_size, y*font_size), density[average], font=my_font, fill=(r, g, b))
-            
     print(original_image_name + " was converted successfully! "+ new_image_name + " was created.")
     new_image.save(new_image_name)
     print("")
